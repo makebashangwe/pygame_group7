0acb3012f074f9475625a0decb89d74b9abc7b52
makebashangwe <makebashangwe@gmail.com>
2024-09-17 17:35:54 -0400

    initialized gamefeature

---
+++ Main/app.py
@@ -0,0 +1,40 @@
+import pygame, sys
+
+class Game:
+    def __init__(self):
+        pygame.init()
+
+        pygame.display.set_caption("Simple Game")
+        self.screen = pygame.display.set_mode((952, 704))
+
+        self.clock = pygame.time.Clock()
+
+        self.img = pygame.image.load("data/images/avatar.png") #avatar
+        self.img_pos = [220,500]
+
+        self.movement = [False,False] #movement variable
+
+    def run(self):
+        while True:
+            self.screen.fill((14,219,248))
+
+            self.img_pos[1] += self.movement[1] - self.movement[0]
+            self.screen.blit(self.img, self.img_pos)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = True
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = False
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = False
+            pygame.display.update()
+            self.clock.tick(60)
+Game().run()
--- README.md
+++
@@ -1,4 +0,0 @@
-This repository contains a group project for a game developed in Python using Pygame. 
-Created as part of an academic assignment, the game is either presented in class or submitted
-via video, depending on course format. The project adheres to strict guidelines: no external 
-libraries, full team collaboration, and original code to avoid plagiarism.
